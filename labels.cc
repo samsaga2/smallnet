ea2593e9246be0636fb41ca228944ddbd986d365
Víctor Marzo Cejudo <samsaga2@gmail.com>
2012-07-13 12:44:54 +0200

    Label bug

--- labels.cc
+++ labels.cc
@@ -20,7 +20,7 @@ string Labels::class_initializer_label(string ns, AST::Class *c) {
 
 string Labels::static_field_label(string ns, AST::Class *c, AST::FieldFeature *f) {
     stringstream ss;
-    ss << "_" << ns << "__" << c->id << "__" << f->id;
+    ss << "_" << boost::replace_all_copy(ns, ".", "_") << "__" << c->id << "__" << f->id;
     return ss.str();
 }
 
